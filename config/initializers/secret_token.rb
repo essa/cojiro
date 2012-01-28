ce6c7cee96f6fcaf28392e569ae30f551270469c
Chris Salzberg <chrissalzberg@gmail.com>
2012-01-28 21:16:02 +0900

    started new rails project with rspec and cucumber.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cojiro::Application.config.secret_token = '00b1086453b0cc3367d6329a1e106c7344f4eb8ccc4204fb890f8cc3658aa36e89d8731d3a18d24017d98ceb857d45d02f1e01cd412016fd23d3f3642eb9311f'
