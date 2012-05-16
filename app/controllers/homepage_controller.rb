320707ceea4dee4f52d9008205536bee5c08423b
Chris Salzberg <chrissalzberg@gmail.com>
2012-05-16 14:39:50 +0900

    index action of homepage controller finds all cothreads.

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,6 +1,12 @@
 class HomepageController < ApplicationController
+  before_filter :find_cothreads, :only => [:index]
 
   def index
   end
 
+  private
+
+  def find_cothreads
+    @cothreads = Cothread.order("created_at DESC").all
+  end
 end
--- spec/controllers/homepage_controller_spec.rb
+++ spec/controllers/homepage_controller_spec.rb
@@ -2,18 +2,39 @@ require 'spec_helper'
 
 describe HomepageController do
 
+  def mock_cothread(stubs={})
+    (@mock_cothread ||= mock_model(Cothread).as_null_object).tap do |cothread|
+      cothread.stub(stubs) unless stubs.empty?
+    end
+  end
+
   describe "GET index" do
 
-    before(:each) { get :index }
-
     it "returns success" do
+      get :index
       response.should be_success
     end
 
     it "renders the index template" do
+      get :index
       response.should render_template("index")
     end
-    
+
+    context "logged-out view" do
+
+      before do
+        Cothread.stub(:order).with("created_at DESC") do
+          double.tap do { |d| d.stub(:all) { [ mock_cothread ] } }
+        end
+      end
+
+      it "assigns cothreads to @cothreads" do
+        get :index
+        assigns(:cothreads).should eq( [ @mock_cothread ] )
+      end
+
+    end
+
   end
 
 end
