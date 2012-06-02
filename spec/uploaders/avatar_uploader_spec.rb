703acafd02d9214d36473f6c423d5fce3b248543
Chris Salzberg <chrissalzberg@gmail.com>
2012-06-03 00:05:47 +0900

    minimal avatar uploader spec.

---
+++ spec/uploaders/avatar_uploader_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+require 'carrierwave/test/matchers'
+
+describe AvatarUploader do
+  include CarrierWave::Test::Matchers
+
+  before do
+    AvatarUploader.enable_processing = true
+    @user = Factory(:user)
+    @uploader = AvatarUploader.new(@user, :avatar)
+    @uploader.store!(File.open(fixture_path + '/' + '400x500.png'))
+  end
+
+  after do
+    AvatarUploader.enable_processing = false
+    @uploader.remove!
+  end
+
+  it "saves the avatar in the correct directory based on the user id" do
+    @uploader.store_dir.should == "uploads/avatars/#{@user.id}"
+  end
+
+  context 'mini version' do
+
+    it 'resizes the avatar to fit into 32 by 32 pixels' do
+      @uploader.mini.should be_no_larger_than(32, 32)
+    end
+
+  end
+
+end
