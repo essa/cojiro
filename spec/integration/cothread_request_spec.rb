ef506800cbda14f5752d6ae57f43a6fd3a2595b5
Chris Salzberg <chrissalzberg@gmail.com>
2012-07-04 10:51:44 +0900

    wrap calls to timecop in do/end.

--- spec/integration/cothread_request_spec.rb
+++ spec/integration/cothread_request_spec.rb
@@ -8,12 +8,15 @@ describe 'Cothreads API' do
 
   describe 'GET "index.json"' do
     before do
-      Timecop.freeze(Time.local(2002,7,20,12,20))
-      cothread = FactoryGirl.create(:cothread, :title => "An old post", :summary => "summary 1", :source_language => "ja", :user => @alice)
-      Timecop.freeze(Time.local(2012,7,20,12,20))
-      cothread = FactoryGirl.create(:cothread, :title => "A very recent post", :summary => "summary 3", :source_language => "ja", :user => @alice)
-      Timecop.freeze(Time.local(2005,7,20,12,20))
-      cothread = FactoryGirl.create(:cothread, :title => "A somewhat recent post", :summary => "summary 2", :source_language => "en", :user => @bob)
+      Timecop.freeze(Time.local(2002,7,20,12,20)) do
+        cothread = FactoryGirl.create(:cothread, :title => "An old post", :summary => "summary 1", :source_language => "ja", :user => @alice)
+      end
+      Timecop.freeze(Time.local(2012,7,20,12,20)) do
+        cothread = FactoryGirl.create(:cothread, :title => "A very recent post", :summary => "summary 3", :source_language => "ja", :user => @alice)
+      end
+      Timecop.freeze(Time.local(2005,7,20,12,20)) do
+        cothread = FactoryGirl.create(:cothread, :title => "A somewhat recent post", :summary => "summary 2", :source_language => "en", :user => @bob)
+      end
 
       get '/en/threads', :format => :json
       @json = JSON(response.body)
