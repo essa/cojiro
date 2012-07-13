425c01952ce3b530896f8cef441702f3e1a223f7
Chris Salzberg <chrissalzberg@gmail.com>
2012-07-13 17:45:24 +0900

    abstract localization strings from layout template.

--- spec/views/layouts/application.html.haml_spec.rb
+++ spec/views/layouts/application.html.haml_spec.rb
@@ -1,30 +1,41 @@
+# encoding: utf-8
 require 'spec_helper'
 
 describe 'layouts/application' do
 
-  # to make sure both logged-in/logged-out test check for
-  # same link text(s)
   before do
-    @start_a_thread = "Start a thread"
-    @twitter_sign_in = "Sign in through Twitter"
+    @start_a_thread =
+      { :en => "Start a thread",
+        :ja => "スレッドを立てる" }
+    @twitter_sign_in =
+      { :en => "Sign in through Twitter",
+        :ja => "Twitterでサインインする" }
   end
 
-  context "English locale" do
-    before { I18n.locale = Globalize.locale = :en }
+  context "logged-out user" do
+    before { view.stub(:logged_in?) { false } }
 
-    context "logged-out user" do
-      before { view.stub(:logged_in?) { false } }
+    shared_examples_for "logged-out user homepage" do |locale|
+      before { I18n.locale = locale }
 
       it "does not render start a thread link" do
         render
-        rendered.should_not have_link(@start_a_thread)
+        rendered.should_not have_link(@start_a_thread[locale])
       end
 
       it "renders twitter sign-in link" do
         render
-        rendered.should have_link(@twitter_sign_in)
+        rendered.should have_link(@twitter_sign_in[locale])
       end
 
+    end
+
+    context "English locale" do
+      it_behaves_like "logged-out user homepage", :en
+    end
+
+    context "Japanese locale" do
+      it_behaves_like "logged-out user homepage", :ja
     end
 
     context "logged-in user" do
@@ -33,14 +44,27 @@ describe 'layouts/application' do
         assign(:current_user, FactoryGirl.create(:user))
       end
 
-      it "renders start a thread link" do
-        render
-        rendered.should have_link(@start_a_thread)
+      shared_examples_for "logged-in user homepage" do |locale|
+        before { I18n.locale = locale }
+
+        it "renders start a thread link" do
+          render
+          rendered.should have_link(@start_a_thread[locale])
+        end
+
+        it "does not render twitter sign-in link" do
+          render
+          rendered.should_not have_link(@twitter_sign_in[locale])
+        end
+
       end
 
-      it "does not render twitter sign-in link" do
-        render
-        rendered.should_not have_link(@twitter_sign_in)
+      context "English locale" do
+        it_behaves_like "logged-in user homepage", :en
+     end
+
+      context "Japanese locale" do
+        it_behaves_like "logged-in user homepage", :ja
       end
 
     end
