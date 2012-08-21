32544844066f54690c83bb3fc447ffc82e675931
Chris Salzberg <chrissalzberg@gmail.com>
2012-08-21 13:39:20 +0900

    speed up rspec tests by increasing logger level.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,6 +21,9 @@ Spork.prefork do
 
   Capybara.javascript_driver = :webkit
 
+  # to speed up tests: http://blog.plataformatec.com.br/2011/12/three-tips-to-improve-the-performance-of-your-test-suite/
+  Rails.logger.level = 4
+
   RSpec.configure do |config|
     include CojiroRequestStubs
 
