0e177f5ba0fc1b4a7b3d762a5e548bd5e01a936f
Taku Nakajima <tnakajima@brain-tokyo.jp>
2012-09-13 14:34:29 +0900

    invoke unicorn from cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,7 @@
+
 set :application, "cojiro"
 set :repository,  "git://github.com/essa/cojiro.git"
+set :rails_env, "production"
 
 set :scm, :git
 set :branch,      'feature/deploy'
@@ -8,7 +10,11 @@ role :web, "localhost"                          # Your HTTP server, Apache/etc
 role :app, "localhost"                          # This may be the same as your `Web` server
 role :db,  "localhost", :primary => true # This is where Rails migrations will run
 
-set :public_children, []
+#set :public_children, []
+
+set :default_environment, {
+  'PATH' => "/home/admin/.rbenv/shims:/home/admin/.rbenv/bin:$PATH"
+}
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -24,3 +30,28 @@ set :public_children, []
 #     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 #   end
 # end
+
+set :config_files, %w(database.yml)
+
+after "deploy:update_code", :role => [:app] do
+  config_files.each do |file|
+    run "cp #{shared_path}/config/#{file} #{release_path}/config/#{file}"
+  end
+end
+
+set :unicorn_pid, "tmp/pids/unicorn.pid"
+
+namespace :deploy do
+  task :start, :roles => :app do
+    run "cd #{current_path}; bundle exec unicorn_rails -c config/unicorn.rb -E #{rails_env} -D"
+  end
+  task :restart, :roles => :app do
+    if File.exist? "#{current_path}/#{unicorn_pid}"
+      run "kill -s USR2 `cat #{current_path}/#{unicorn_pid}`"
+    end
+  end
+  task :stop, :roles => :app do
+    run "kill -s QUIT `cat #{current_path}/#{unicorn_pid}`"
+  end
+end
+
