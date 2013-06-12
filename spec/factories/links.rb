6b24a56f082dc3010fbe1ab1f8618c653aa7f62e
Chris Salzberg <chrissalzberg@gmail.com>
2013-06-12 14:37:14 +0900

    generate urls dynamically in link factory to avoid failing uniqueness validation.

--- spec/factories/links.rb
+++ spec/factories/links.rb
@@ -1,8 +1,10 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
+  sequence(:url) { |n| "http://www.mywebsite#{n}.com" }
+
   factory :link do
-    url "http://www.mywebsite.com/"
+    url { FactoryGirl.generate(:url) }
     after(:build) { |l| l.user ||= FactoryGirl.create(:alice) }
     after(:stub) { |l| l.user ||= FactoryGirl.build_stubbed(:alice) }
   end
