f696a48880d6483e407eed11440f8ffd4064de8d
Chris Salzberg <chrissalzberg@gmail.com>
2013-06-12 12:38:30 +0900

    include link in comment JSON.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ActiveRecord::Base
   belongs_to :cothread
   belongs_to :link
   attr_accessible :text
+
+  def as_json(options = {})
+    json = super(options.merge(:include => [ :link ]))
+  end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -6,4 +6,17 @@ describe Comment do
     it { should belong_to(:link) }
     it { should belong_to(:cothread) }
   end
+
+  describe '#to_json' do
+    before do
+      @comment = FactoryGirl.build(:comment,
+                                   link: FactoryGirl.create(:link))
+      @comment_json = @comment.to_json
+    end
+
+    it 'includes link' do
+      JSON(@comment_json)['link'].should be
+      JSON(@comment_json)['link']['url'].should be
+    end
+  end
 end
