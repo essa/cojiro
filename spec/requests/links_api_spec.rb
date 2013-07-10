d98c87e38a29971a1ec6dc08664bfb4d6ba2a175
Chris Salzberg <chrissalzberg@gmail.com>
2013-07-10 16:46:08 +0900

    use a more difficult url in links request spec to make sure things are working correctly.

--- spec/requests/links_api_spec.rb
+++ spec/requests/links_api_spec.rb
@@ -1,32 +1,32 @@
+# encoding: utf-8
 require 'spec_helper'
 
 describe 'Links API' do
-  use_vcr_cassette('what_is_crossfit')
+  use_vcr_cassette('capoeira_wikipedia_ja')
 
   #TODO: add more API specs, this is just a minimum to make sure URL passed in through
   # query string is correctly parsed (with loosed constraint in rails route) and
   # parsed heuriscially (e.g. adding 'http://') to get the canonical url for the new link
   describe 'GET /en/links/<id>.json' do
     context 'record with id = <id> exists' do
-      let(:link) { FactoryGirl.create(:link, url: 'youtu.be/tzD9BkXGJ1M') }
+      let(:link) { FactoryGirl.create(:link, url: 'ja.wikipedia.org/wiki/カポエイラ') }
       let(:json) { JSON(response.body) }
-      before { get "/en/links/#{URI.decode_www_form_component(link.url)}", :format => :json }
+      before { get "/en/links/#{link.url}", :format => :json }
 
       it 'returns with correct response code' do
         response.response_code.should == 200
       end
 
       it 'returns link with normalized url' do
-        json.should include('url' => 'http://youtu.be/tzD9BkXGJ1M')
+        json.should include('url' => 'http://ja.wikipedia.org/wiki/%E3%82%AB%E3%83%9D%E3%82%A8%E3%82%A4%E3%83%A9')
       end
 
       it 'returns embed data' do
         json.should have_key('embed_data')
         embed_data = json['embed_data']
-        embed_data.should include('title' => 'What is CrossFit?')
-        embed_data.should include('description' => 'What is CrossFit? CrossFit is an effective way to get fit. Anyone can do it. It is a fitness program that combines a wide variety of functional movements into a timed or scored workout. We do pull-ups, squats, push-ups, weightlifting, gymnastics, running, rowing, and a host of other movements.')
-        embed_data.should include('height' => 480)
-        embed_data.should include('width' => 854)
+        embed_data.should include('title' => 'カポエイラ')
+        embed_data.should include('thumbnail_height' => 208)
+        embed_data.should include('thumbnail_width' => 300)
       end
     end
   end
